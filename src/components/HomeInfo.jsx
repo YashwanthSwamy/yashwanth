2509fdc83e922b74ab9faa192bf9dd77a70270d9
YashwanthSwamy <yasw2875@colorado.edu>
2023-11-13 13:25:53 -0700

    feat: info box for home and icons for the project

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,52 @@
+import {Link} from 'react-router-dom';
+import arrow from '../assets/icons/arrow.svg';
+
+const InfoBox = ({content, link, btnText}) => {
+  return (
+  <div className='info-box'>
+    <p className='font-medium sm:text-m text-center'>{content}</p>
+    <Link to={link} className='neo-brutalism-white neo-btn'>
+        {btnText}
+        <img src={arrow} alt='arrow-right' className='w-4 h-4 object-contain'/>
+    </Link>
+  </div>
+  )
+};
+
+const stageContent = {
+    1: (
+        <h1 className='sm:text-l sm:leading-snug text-center neo-brutalism-blue text-white mx-5 py-4 px-8'>
+            Hi! 👋 
+            I am <span className='font-semibold'>Yashwanth Swamy</span> 
+            <br/>
+            A Software Engineer From States
+        </h1>
+    ),
+    2: (
+        <InfoBox 
+        content="Worked as a Senior Software engineer in Siemens and Software engineer Intern at Nasdaq"
+        link='/about'
+        btnText='Know More'
+        />
+    ),
+    3: (
+        <InfoBox 
+        content="Led multiple projects and worked on various technologies. Curious about my projects?"
+        link='/projects'
+        btnText='See Projects'
+        />
+    ),
+    4: (
+        <InfoBox 
+        content="Looking for a Developer? I'm just a click away."
+        link='/contact'
+        btnText="Let's talk"
+        />
+    ),
+}
+
+const HomeInfo = ({ currentStage }) => {
+    return stageContent[currentStage];
+}
+
+export default HomeInfo
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,18 @@ import Island from '../models/Island';
 import Sky from '../models/sky';
 import Bird from '../models/Bird';
 import Plane from '../models/Plane';
-
-{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-    POPUP
-</div> */}
+import HomeInfo from '../components/HomeInfo';
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(0);
 
   const adjustIslandForScreenSize = () => {
-    let  screenScale = [];
+    let screenScale = [];
     let screenPosition = [0, -6.5, -43];
     let rotation = [0.1, 4.7, 0];
 
-    if(window.innerWidth < 768){
+    if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
     } else {
       screenScale = [1, 1, 1];
@@ -29,9 +26,9 @@ const Home = () => {
   }
 
   const adjustPlaneForScreenSize = () => {
-    let  planePosition, planeScale;
+    let planePosition, planeScale;
 
-    if(window.innerWidth < 768){
+    if (window.innerWidth < 768) {
       planeScale = [1.5, 1.5, 1.5];
       planePosition = [0, -1.5, 0];
     } else {
@@ -42,46 +39,49 @@ const Home = () => {
     return [planePosition, planeScale];
   }
 
-  const  [islandPosition, islandScale, islandRotation] = adjustIslandForScreenSize();
+  const [islandPosition, islandScale, islandRotation] = adjustIslandForScreenSize();
   const [planePosition, planeScale] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
-        <Canvas 
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo currentStage={currentStage}/>}
+      </div>
+      <Canvas
         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'} `}
-        camera={{near: 0.1, far: 1000}}
-        >
-            <Suspense fallback={<Loader/>}>
-                <directionalLight 
-                position={[1,1,1]}
-                intensity={1.5}
-                /> {/* like sun  */}
-                <ambientLight intensity={1}/>  {/* no shadow and adds light to all  */}
-                {/* <pointLight/>  */} {/* emits light in all directions similar to ambientLight but in an angle  */}
-                {/* <spotLight/> similar ro pointLight but in shape of a cone*/}
-                <hemisphereLight 
-                skyColor='#1be1ff' 
-                groundColor='#000000' 
-                intensity={0.5}
-                />  {/* iluiminates with a gradient like sky  adds detail to the scene */}
-                <Sky isRotating= {isRotating}/>
-                <Bird/>
-                <Island
-                position= {islandPosition}
-                scale= {islandScale}
-                rotation = {islandRotation}
-                isRotating= {isRotating}
-                setIsRotating={setIsRotating}
-                setCurrentStage={setCurrentStage}
-                />
-                <Plane
-                planePosition={planePosition}
-                planeScale={planeScale}
-                isRotating={isRotating}
-                rotation={[0,20,0]}
-                />
-            </Suspense>
-        </Canvas>
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight
+            position={[1, 1, 1]}
+            intensity={1.5}
+          /> {/* like sun  */}
+          <ambientLight intensity={1} />  {/* no shadow and adds light to all  */}
+          {/* <pointLight/>  */} {/* emits light in all directions similar to ambientLight but in an angle  */}
+          {/* <spotLight/> similar ro pointLight but in shape of a cone*/}
+          <hemisphereLight
+            skyColor='#1be1ff'
+            groundColor='#000000'
+            intensity={0.5}
+          />  {/* iluiminates with a gradient like sky  adds detail to the scene */}
+          <Sky isRotating={isRotating} />
+          <Bird />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+          />
+          <Plane
+            planePosition={planePosition}
+            planeScale={planeScale}
+            isRotating={isRotating}
+            rotation={[0, 20, 0]}
+          />
+        </Suspense>
+      </Canvas>
     </section>
   )
 }
